b3b55bf6428935ae3a78fb3362af5b3970103978
SirSlackerKing <suppakij.j17@gmail.com>
2026-03-16 19:22:29 +0700

    Create analytics-goatcounter.js

---
+++ analytics-goatcounter.js
@@ -0,0 +1,11 @@
+var s = document.createElement("script");
+
+s.async = true;
+s.src = "//gc.zgo.at/count.js";
+
+s.setAttribute(
+"data-goatcounter",
+"https://sirslackerking.goatcounter.com/count"
+);
+
+document.head.appendChild(s);
